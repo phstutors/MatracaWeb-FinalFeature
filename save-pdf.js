cdfdeecb90cac9e340f30f46725ac202c01b96fa
PhsTutors <phs3@discente.ifpe.edu.br>
2023-05-11 20:56:08 -0300

    Add nova funcionalidade

--- save-pdf.js
+++ save-pdf.js
@@ -10,3 +10,21 @@ function  funcao_pdf(){
     janela.document.close();
     janela.print();
 }
+function darkMode(){
+
+    document.getElementById('mudar').style.background= "black";
+    document.getElementById('mudar').style.color= "white";
+    document.getElementById('mudar2').style.background= "black";
+    document.getElementById('mudar2').style.color= "white";
+
+}
+function lightMode(){
+
+    document.getElementById('mudar').style.background= "#dee0df";
+    document.getElementById('mudar').style.color= "black";
+    document.getElementById('mudar2').style.background= "#dee0df";
+    document.getElementById('mudar2').style.color= "black";
+
+}
+
+  
